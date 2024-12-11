f01d09126a221fd30c94f9117a407b5f6c8261da
Hustbiao2022 <128591980+Hustbiao2022@users.noreply.github.com>
2024-12-11 16:27:51 +0800

    Add files via upload

---
+++ matlab_reconstruction/get_correction.m
@@ -0,0 +1,20 @@
+function correction_matrix = get_correction(mode, factor, theta)
+
+    c = cos(theta);
+    s = sin(theta);
+
+
+    
+    if mode
+        correction_matrix = [-c              0  s            0
+                             0               1  0            0
+                             s               0  c            0
+                             0               0  0            1];
+    else
+        correction_matrix = [0               1  0            0
+                             -c              0  s            0
+                             s               0  c            0
+                             0               0  0            1];
+    end
+
+end
